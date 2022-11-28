75ab5ca0ecfd05245ed94c945c1608d0272985ce
puritymwaura <purity.mwaura@student.moringaschool.com>
2022-11-28 11:01:19 +0300

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
+    validate :title_is_clickbait
+
+    def title_is_clickbait 
+        if title == "True Facts" 
+            errors.add(:title, "Invalid") 
+        end
+    end
+
+
 end
